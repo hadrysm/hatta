5892f20f3e71f468f713bf096d8d36a23b69be78
Mateusz Hadryś <m.hadrys.95@gmail.com>
2020-11-08 21:12:02 +0100

    Add ThemeProvider

--- src/templates/MainTemplate/MainTemplate.js
+++ src/templates/MainTemplate/MainTemplate.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { ThemeProvider } from 'styled-components';
 
 import GlobalStyled from 'assets/styles/GlobalStyles';
+import { theme } from 'assets/styles/mainTheme';
 
 const MainTemplate = ({ children }) => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyled />
       {children}
-    </>
+    </ThemeProvider>
   );
 };
 

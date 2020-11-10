0c5e71910db00b8363cea078c94eaefc48b3fd53
Mateusz Hadryś <m.hadrys.95@gmail.com>
2020-11-10 18:23:56 +0100

    Add input component

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import SEO from 'components/utilities/SEO/SEO';
 import HeadlineContent from 'components/molecules/HeadlineContent/HeadlineContent';
+import Input from 'components/atoms/Input/Input';
 
 const concactHeadline = {
   title: 'concact',
@@ -13,6 +14,16 @@ const ContactPage = () => (
   <>
     <SEO title="Home" />
     <HeadlineContent title={concactHeadline.title} paragraph={concactHeadline.paragraph} />
+    <Input type="text" name="name" label="name" value="Adam" onChange={() => {}} />
+    <Input type="email" name="email" label="e-mail" value="adam@gmail.com" onChange={() => {}} />
+    <Input
+      as="textarea"
+      type="text"
+      name="message"
+      label="message"
+      value="PM me"
+      onChange={() => {}}
+    />
   </>
 );
 
